6decd380b73a71f62689e851d6f7b0b02f331b00
Lucy Brandão <lucyjbrandao@gmail.com>
2022-08-04 15:05:55 -0300

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # robotron-2000
+
+<!-- ![My robotron-2000](././img/robotron_rosa.png) -->
+
+## General info
+A project from "Javascript: manipulando o DOM - Alura" curse. 
+
+## Technologies
+Project is created with:
+* Javascript
+* HTML5
+* CSS3
+
+## Setup
+Just download this project and open `index.html` in your browser.
+
+font: 'https://www.alura.com.br/curso-online-javascript-manipulacao-dom'
--- js/main.js
+++ js/main.js
@@ -1,6 +1,58 @@
-const robotron = document.querySelector('#robotron');
-robotron.addEventListenner('click', eleGosta);
+const controle = document.querySelectorAll('[data-controle]');
+const estatisticas = document.querySelectorAll('[data-estatistica]')
+const pecas = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
 
-function eleGosta() {
-    console.log('uuuuiii');
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos":{
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -24
+    },
+    "pernas":{
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 42
+    },
+    "foguetes":{
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
+    }
+}
+
+controle.forEach((elemento) => {
+    elemento.addEventListener('click', (evento) => {
+        manipulaDados(evento.target.dataset.controle, evento.target.parentNode)
+        atualizaEstatitsticas(evento.target.dataset.peca)
+    })
+});
+
+function manipulaDados(operacao, controle) {
+    const peca = controle.querySelector('[data-contador]')
+
+    if(operacao === '-') {
+        peca.value = parseInt(peca.value) - 1
+    } else {
+        peca.value = parseInt(peca.value) + 1
+    }
+}
+
+function atualizaEstatitsticas(peca) {
+    estatisticas.forEach((elemento) => {
+        elemento.textContent = parseInt(elemento.textContent) + pecas[peca][elemento.dataset.estatistica]
+    })
 }
